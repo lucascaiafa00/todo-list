8b7d96bc321e22a2a3c7c4d6a508188d8ef47421
Lucas Caiafa <100371782+lucascaiafa00@users.noreply.github.com>
2022-08-02 17:02:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let addTodoInput = document.getElementById("add-todo-input")
 let addTodoButton = document.getElementById("add-todo-button")
 let todoList = document.getElementById("todo-list")
+let checkboxNumber = 0
 
 document.addEventListener('keypress', function(e){
     var key = e.which || e.keyCode;
@@ -13,21 +14,24 @@ addTodoButton.onclick = ()=>{
 }
 function addTodo() {
     if (addTodoInput.value != "") {
-        
         todoList.innerHTML += 
         `<li>
-        <input type="checkbox" name="" id="checkbox">
-        <label for="checkbox">${addTodoInput.value}</label>
+        <input type="checkbox" name="" id="checkbox${checkboxNumber}">
+        <label for="checkbox${checkboxNumber}">${addTodoInput.value}</label>
         <img src="trash-bin.png" alt="remove" onclick="removeTodo(this)">
         </li>`
+
         addTodoInput.value = ""
         addTodoInput.placeholder = "Add to-do"
+
+        checkboxNumber += 1
     }else{
         addTodoInput.placeholder = "Type something first"
     }
 }
 function removeTodo(elem) {
     elem.parentNode.remove()
+    checkboxNumber = 0
 }
 function removeAll() {
     todoList.innerHTML = ""
